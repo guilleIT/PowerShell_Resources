467301e3f909b51a89d87374619156861dcc822d
GuilleDev <guillermo.unzueta@gmail.com>
2025-07-23 15:16:33 +0200

    New

--- Read_3_last_lines.ps1
+++ Read_3_last_lines.ps1
@@ -21,3 +21,29 @@ Get-ChildItem -Path $logFolder -Filter "*.log" | ForEach-Object {
         Write-Host "Archivo '$file': No tiene suficientes líneas"
     }
 }
+
+
+$logFolder = "\\rdfst1-de-hub-1\REE1-DE-HUB-1_PersistentFolder\user\YEPermissionAnalyserLogs" # Cambia esta ruta si es necesario
+
+
+
+Get-ChildItem -Path $logFolder -Filter "*.log" | ForEach-Object {
+    $file = $_.FullName
+    $lines = Get-Content -Path $file
+    if ($lines.Count -ge 3) {
+        # Tomar las últimas 3 líneas
+        $last3 = $lines[-3..-1]
+
+        $antepenultimateOK = $last3[0] -match "\.\.OK!"
+        $penultimateOK     = $last3[1] -match "\.\.OK!"
+        $lastMinutes       = $last3[2] -match "Minutes"
+
+        if ($antepenultimateOK -and $penultimateOK -and $lastMinutes) {
+            Write-Host "File '$file': Meets the conditions"
+        } else {
+            Write-Host "File '$file': Does not meet the conditions"
+        }
+    } else {
+        Write-Host "File '$file': Does not have enough lines"
+    }
+}
